a63374fbf1c45ade30bb7d029fe17207d4a1e484
Felipe Calderaro <j.felipe.calderaro@gmail.com>
2025-11-22 23:05:15 -0300

    FDIO-65: Tabs, Product improvements

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default () => ({
+    graphql: {
+        enabled: true,
+        config: {
+            endpoint: '/graphql',
+            shadowCRUD: true,
+            playgroundAlways: true,
+            depthLimit: 10,
+            amountLimit: 100,
+            apolloServer: {
+                tracing: true,
+            },
+        },
+    },
+});
---
+++ src/api/aba/controllers/aba.ts
@@ -0,0 +1,7 @@
+/**
+ * aba controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::aba.aba');
